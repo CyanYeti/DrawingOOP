7f92b922dcaa0339bc2dbfa3da72eb084864c20a
CyanYeti <3tferney@gmail.com>
2023-03-15 06:43:02 -0600

    Init

---
+++ AppLayer/DrawingComponents/Background.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLayer.DrawingComponents
+{
+    public abstract class Background
+    {
+        public abstract T GetBackground<T>();
+    }
+}
---
+++ AppLayer/DrawingComponents/BackgroundColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace AppLayer.DrawingComponents
+{
+    internal class BackgroundColor : Background
+    {
+        public Color color { get; set; }
+
+        public T GetBackground<T>()
+        {
+            return color;
+        }
+
+        
+    }
+}
